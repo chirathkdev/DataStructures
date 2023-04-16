60974b92bd0fbe6267011b1bc557291c13403d0c
chirathkdev <chirathkdev@gmail.com>
2023-04-16 12:27:40 +0530

    Initial Commit

---
+++ DataStructures/DataStructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataStructuresApp.swift
+//  DataStructures
+//
+//  Created by Chirath Kumarasiri on 2023-04-16.
+//
+
+import SwiftUI
+
+@main
+struct DataStructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
